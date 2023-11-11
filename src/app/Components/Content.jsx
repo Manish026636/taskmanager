e9cedc91a486d06d8fa280feb8baa6e7349e620e
Manish <my79220@gmail.com>
2023-11-11 18:24:05 +0530

    added

--- src/app/Components/Content.jsx
+++ src/app/Components/Content.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import TaskDashboard from './Tasks/TaskDashboard'
 import Profile from './Profile/Profile'
+import Dashboard from './Dashboard'
 
 const Content = () => {
   return (
     <div>
-      <Profile/>
+   <Profile/>
     </div>
   )
 }
--- src/app/Components/Profile/Profile.jsx
+++ src/app/Components/Profile/Profile.jsx
@@ -3,38 +3,44 @@ import React from 'react'
 const Profile = () => {
     return (
         <div>
-            <div className='  px-6'>
-                <div className='flex justify-center mt-32'>
-                    <div className='bg-white border border-gray-200 rounded-[30px] w-auto  h-auto py-10'>
-                        <div className='flex px-10 justify-center mt-2'>
-                            <img src='https://images.unsplash.com/photo-1699581695777-167db9ac6e23?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' className='w-28 h-28 rounded-full' alt='' />
-                        </div>
-                        <h1 className='text-3xl px-10 text-center mt-4 font-bold text-gray-400'>Lakshitaa Chellaramani</h1>
-                        <div className='border border-blue-100 mt-4'></div>
-                        <div className='flex px-8 pt-6 justify-start'>
-                            <img src='email.svg ' className='w-6 h-6' alt='' />
-                            <h1 className=' text-[15px] ml-4 mt-1 text-gray-400 uppercase font-medium'>Email Id : <span className='text-md text-gray-500 font-bold'>lakshitaa@gmail.com</span></h1>
-                            
-                        </div>
-                        <div className='flex px-8 pt-6 justify-start'>
-                            <img src='calendar.svg' className='w-6 h-6' alt='' />
-                            <h1 className=' text-[15px] ml-4 text-gray-400 uppercase font-medium'>Year : <span className='text-md text-gray-500 font-bold'>BTECH - 2ND YEAR</span></h1>
-                            
-                        </div>
-                        <div className='flex px-8 pt-6 justify-start'>
-                            <img src='ID.svg' className='w-6 h-6' alt='' />
-                            <h1 className=' text-[15px] ml-4 text-gray-400 uppercase font-medium'>SAP ID : <span className='text-md text-gray-500 font-bold'>80289333</span></h1>
-                            
-                        </div>
-                        <div className='flex px-8 pt-6 justify-start'>
-                            <img src='committee.svg' className='w-6 h-6' alt='' />
-                            <h1 className=' text-[15px] ml-4 text-gray-400 uppercase font-medium'>Committee : <span className='text-md text-gray-500 font-bold'>GDSC</span></h1>
-                            
-                        </div>
+            <div className='flex justify-center '>
+
+
+                <div className='w-full h-full'>
+                    <div className=' w-full  h-48'>
+                        <img src='https://images.unsplash.com/photo-1555679427-1f6dfcce943b?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' className='w-full h-full object-cover' />
+                    </div>
+                    <div>
+                        <img src='https://images.unsplash.com/photo-1620052569626-dcbc8ee3a269?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' className='w-36 object-cover  h-36 rounded-full mx-auto lg:mx-10 -mt-20 border-4 border-white' />
 
                     </div>
-                </div>
+                    <div>
+                        <h1 className='lg:mx-10 text-center lg:text-left mt-4 text-lg font-bold text-gray-500'>Manishkumar Yadav</h1>
+                    </div>
+                    <div className='flex justify-center '>
+                        <div className='grid mt-14 lg:mt-20 gap-4 lg:gap-8 lg:grid-cols-3'>
+                            <div className='bg-gray-50  flex w-auto px-2 h-16 rounded-xl border'>
+                                <img src='email.svg' alt='' className='w-6 h-6  my-5' />
+                                <h1 className='text-md  my-5 ml-1 text-gray-500'>Email : <span className='font-bold'>ymanishkumar@gmail.com</span></h1>
+                            </div>
+                            <div className='bg-gray-50  flex w-auto px-2 h-16 rounded-xl border'>
+                                <img src='calender.svg' alt='' className='w-6 h-6  my-5' />
+                                <h1 className='text-md  my-5 ml-1 text-gray-500'>Year : <span className='font-bold'>BTECH - 2nd Year</span></h1>
+                            </div>
+                            <div className='bg-gray-50  flex w-auto px-2 h-16 rounded-xl border'>
+                                <img src='ID.svg' alt='' className='w-6 h-6  my-5' />
+                                <h1 className='text-md  my-5 ml-1 text-gray-500'>SAP ID : <span className='font-bold'>727882</span></h1>
+                            </div>
+                            <div className='bg-gray-50  flex w-auto px-2 h-16 rounded-xl border'>
+                                <img src='committee.svg' alt='' className='w-6 h-6  my-5' />
+                                <h1 className='text-md  my-5 ml-1 text-gray-500'>Committee : <span className='font-bold'>Something</span></h1>
+                            </div>
+                            
+                        </div>
+                    </div>
 
+
+                </div>
             </div>
         </div>
     )
